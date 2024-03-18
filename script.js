0c313aec577519c6c1ea4eb6ac9f41b4a19e2acc
NathanHu2024 <159183463+NathanHu2024@users.noreply.github.com>
2024-03-18 14:06:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function greeter() {
-  let name = prompt("The purpose of this website is to inform you about Bill Gates' significant contributions to conputer science. Have you heard of Bill Gates?");
+  let name = prompt("The purpose of this website is to inform you about Bill Gates' significant contributions to computer science. Have you heard of Bill Gates?");
   document.getElementById("greet").innerHTML =
-  "Your response " +name+ " is great";
+  "Your response " +name+ " " "is great";
 }
